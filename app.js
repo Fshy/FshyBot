258d331644d2f3fa13e77ee392975a7d4a1f3c3a
Fshy <akdesignz@outlook.com>
2017-03-18 02:05:32 -0400

    Modularize Commands

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const request = require('request');
 const Discord = require('discord.js');
 const music   = require('discord.js-music-v11');
 const config  = require('./config.json');
+const commands= require('./commands');
 const bot     = new Discord.Client();
 
 const token   = config.token;
@@ -23,164 +24,58 @@ bot.on('message', (message)=>{
   if(!message.content.startsWith(prefix)) return;
   console.log("-- "+message.author.username+": "+message.content);
 
-  let command = message.content.split(" ")[0];
+  let command = message.content.split(/\s+/g)[0];
   command = command.slice(prefix.length);
-  let args = message.content.split(" ").slice(1);
+  let args = message.content.split(/\s+/g).slice(1);
 
   // !help = Displays all available commands
   if(command === 'help'){
-    message.channel.sendMessage('Commands: \`!help !ping !uptime !btc !eth\`');
+    commands.help(message);
   }else
 
-  // !ping = Displays latency between user and bot
+  // !ping = Displays latency between the bot and the server
   if(command === 'ping'){
     message.channel.sendMessage('Response Time between '+message.author+' and '+name+': \`'+(Date.now()-message.createdTimestamp)+'ms\`');
-
   }else
 
   // !uptime = Displays time since launch
   if(command === 'uptime'){
-    var time = Date.now()-launchTime;
-    time = parseInt(time/1000);
-    var seconds = time % 60;
-    time = parseInt(time/60);
-    var minutes = time % 60;
-    time = parseInt(time/60);
-    var hours = time % 24;
-    message.channel.sendMessage('Uptime: \`'+hours+' hrs '+minutes+' mins '+seconds+' sec\`');
+    commands.uptime(launchTime,message);
   }else
 
   // !btc - Displays current Bitcoin spot price
   if(command === 'btc'){
-    request('https://coinmarketcap-nexuist.rhcloud.com/api/btc', function (error, response, body) {
-      response = JSON.parse(body);
-      if (error!=null) {
-        message.channel.sendMessage('\`ERROR: Could not access coinmarketcap API\`');
-      }else {
-        message.channel.sendMessage('Current BTC Price: \`$'+response.price.usd.toFixed(2)+'\`');
-      }
-    });
+    commands.btc(message);
   }else
 
   // !eth - Displays current Ethereum spot price
   if(command === 'eth'){
-    request('https://coinmarketcap-nexuist.rhcloud.com/api/eth', function (error, response, body) {
-      response = JSON.parse(body);
-      if (error!=null) {
-        message.channel.sendMessage('\`ERROR: Could not access coinmarketcap API\`');
-      }else {
-        message.channel.sendMessage('Current ETH Price: \`$'+response.price.usd.toFixed(2)+'\`');
-      }
-    });
+    commands.eth(message);
   }else
 
-  // !search - Utilizes DDG API for an instant answer
+  // !search <term> - Utilizes DDG API for an instant answer
   if(command === 'search'){
-    var a = args.join("%20");
-    request('http://api.duckduckgo.com/?q='+a+'&format=json', function (error, response, body) {
-      body = JSON.parse(body);
-      if (error!=null) {
-        message.channel.sendMessage('\`ERROR: Could not access DuckDuckGo API\`');
-      }else {
-        // console.log(response);
-        if (body.Abstract) {
-          message.channel.sendMessage('```Markdown\n#'+body.Heading+':\n'+body.Abstract+'```');
-        }else {
-          message.channel.sendMessage('\`No Instant Answer Available\`');
-        }
-      }
-    });
+    commands.search(args,message);
   }else
 
-  // !playlist <playlistId> - Utilizes DDG API for an instant answer
+  // !playlist <playlistId> - Queues all videos from a youtube playlist
   if(command === 'playlist'){
-    var list = args[0];
-    request('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key='+config.googleAPIkey+'&maxResults=50&playlistId='+list, function (error, response, body) {
-      body = JSON.parse(body);
-      if (error!=null) {
-        message.channel.sendMessage('\`ERROR: Could not access Google API\`');
-      }else {
-        message.member.voiceChannel.join();
-        for (var i = 0; i < body.items.length; i++) {
-          message.channel.sendMessage('!play '+body.items[i].snippet.resourceId.videoId).then(message => message.delete());
-        }
-      }
-    });
+    commands.playlist(args,message);
   }
 
   // !register - Registers user to db
   if(command === 'register'){
-    request('https://api.myjson.com/bins/'+config.jsonDB, function (error, response, body) {
-      res = JSON.parse(body);
-      var exists = false;
-      if (error!=null) {
-        message.channel.sendMessage('\`ERROR: Could not access database\`');
-      }else {
-        for (var i = 0; i < res.user.length; i++) {
-          if (res.user[i].id===message.author.id) {
-            message.channel.sendMessage('\`ERROR: User already exists in database\`');
-            exists = true;
-          }
-        }
-        if (!exists) {
-          res.user.push({id: message.author.id, name:message.author.username, currency:5000});
-          // res = JSON.stringify(res);
-          request({ url: 'https://api.myjson.com/bins/'+config.jsonDB, method: 'PUT', json: res});
-          message.channel.sendMessage('Registered new wallet for @'+message.author.username);
-        }
-      }
-    });
+    commands.register(message);
   }
 
-  // !getbal <username> - Finds user balance
-  if(command === 'getbal'){
-    console.log('https://api.myjson.com/bins/'+config.jsonDB);
-    request('https://api.myjson.com/bins/'+config.jsonDB, function (error, response, body) {
-      res = JSON.parse(body);
-      if (error!=null) {
-        message.channel.sendMessage('\`ERROR: Could not access database\`');
-      }else {
-        if (args[0]) {
-          var id = args[0].replace(/\D/g, '');
-          for (var i = 0; i < res.user.length; i++) {
-            if (res.user[i].id===id) {
-              message.channel.sendMessage('Currency: $'+res.user[i].currency);
-            }
-          }
-        }else{
-          for (var i = 0; i < res.user.length; i++) {
-            if (res.user[i].id===message.author.id) {
-              message.channel.sendMessage('Currency: $'+res.user[i].currency);
-              console.log(res.user[i].currency);
-            }
-          }
-        }
-
-      }
-    });
+  // !wallet <username> - Finds user balance
+  if(command === 'wallet'){
+    commands.wallet(args,message);
   }else
 
-  // !addbal <username> <amount> - Adds to User
-  if(command === 'addbal'){
-    request('https://api.myjson.com/bins/'+config.jsonDB, function (error, response, body) {
-      var res = JSON.parse(body);
-      if (error!=null) {
-        message.channel.sendMessage('\`ERROR: Could not access database\`');
-      }else {
-        if (args.length!=2) {
-          message.channel.sendMessage('\`ERROR: Incorrect Format => !addbal <@user> <amount>\`');
-        }else {
-          var id = args[0].replace(/\D/g, '');
-          for (var i = 0; i < res.user.length; i++) {
-            if (res.user[i].id===id) {
-              res.user[i].currency += parseInt(args[1]);
-              message.channel.sendMessage('Added $'+parseInt(args[1])+' to '+message.mentions.users.get(id)+'\'s account | New Balance: $'+res.user[i].currency)
-              .then(request({ url: 'https://api.myjson.com/bins/'+config.jsonDB, method: 'PUT', json: res }));
-            }
-          }
-        }
-      }
-    });
+  // !walletadd <username> <amount> - Adds to User
+  if(command === 'walletadd'){
+    commands.walletadd(args,message);
   }
 
 });
---
+++ commands.js
@@ -0,0 +1,178 @@
+const request = require('request');
+const config  = require('./config.json');
+
+module.exports = {
+  help: function (message) {
+    return message.channel.sendEmbed({
+        description: `__**2B | FshyBot**__
+
+          __**Rules:**__
+          1. This is an assignment-free zone
+          2. If your mic echoes, you're fucking banned
+          ~~3. No loitering in the lobby~~
+          ~~4. No asking for Server Icons~~
+          ~~5. If you have to ask for icons, ask Fshy~~
+          6. Don't be a faggot
+          7. Get Gud
+
+          __**FAQ:**__
+          ???
+
+          __**Commands:**__
+          **!help**\t- Displays all available commands.
+          **!ping**\t- Displays latency between the bot and the server.
+          **!uptime**\t- Displays time since launch.
+
+          **!btc**\t- Displays current Bitcoin spot price
+          **!eth**\t- Displays current Ethereum spot price
+          **!search \<term\>**\t- Utilizes DuckDuckGo API for an instant answer
+
+          **!play \<search term | link\>**\t- Searches and queues the given term/link for playback
+          **!playlist \<playlistId\>**\t- Queues all videos from a youtube playlist
+          **!skip \<number\>**\t- Skip some number of songs or 1 song if a number is not specified
+          **!queue**\t- Display the current queue
+          **!leave**\t- Clears the song queue and leaves the channel
+
+          For source code and other dank memes check [arc.moe](https://arc.moe)`,
+        color: 15473237
+      });
+  },
+  uptime: function (launchTime,message) {
+    var time = Date.now()-launchTime;
+    time = parseInt(time/1000);
+    var seconds = time % 60;
+    time = parseInt(time/60);
+    var minutes = time % 60;
+    time = parseInt(time/60);
+    var hours = time % 24;
+    message.channel.sendMessage('Uptime: \`'+hours+' hrs '+minutes+' mins '+seconds+' sec\`');
+  },
+  btc: function (message) {
+    request('https://coinmarketcap-nexuist.rhcloud.com/api/btc', function (error, response, body) {
+      response = JSON.parse(body);
+      if (error!=null) {
+        message.channel.sendMessage('\`ERROR: Could not access coinmarketcap API\`');
+      }else {
+        message.channel.sendMessage('Current BTC Price: \`$'+response.price.usd.toFixed(2)+'\`');
+      }
+    });
+  },
+  eth: function (message) {
+    request('https://coinmarketcap-nexuist.rhcloud.com/api/eth', function (error, response, body) {
+      response = JSON.parse(body);
+      if (error!=null) {
+        message.channel.sendMessage('\`ERROR: Could not access coinmarketcap API\`');
+      }else {
+        message.channel.sendMessage('Current ETH Price: \`$'+response.price.usd.toFixed(2)+'\`');
+      }
+    });
+  },
+  search: function (args,message) {
+    var a = args.join("%20");
+    request('http://api.duckduckgo.com/?q='+a+'&format=json', function (error, response, body) {
+      body = JSON.parse(body);
+      if (error!=null) {
+        message.channel.sendMessage('\`ERROR: Could not access DuckDuckGo API\`');
+      }else {
+        if (body.Abstract) {
+          message.channel.sendMessage('```Markdown\n#'+body.Heading+':\n'+body.Abstract+'```');
+        }else {
+          message.channel.sendMessage('\`No Instant Answer Available\`');
+        }
+      }
+    });
+  },
+  playlist: function (args,message) {
+    var list = args[0];
+    request('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key='+config.googleAPIkey+'&maxResults=50&playlistId='+list, function (error, response, body) {
+      body = JSON.parse(body);
+      if (error!=null) {
+        message.channel.sendMessage('\`ERROR: Could not access Google API\`');
+      }else {
+        message.member.voiceChannel.join();
+        for (var i = 0; i < body.items.length; i++) {
+          message.channel.sendMessage('!play '+body.items[i].snippet.resourceId.videoId).then(message => message.delete());
+        }
+      }
+    });
+  },
+  register: function (message) {
+    request('https://api.myjson.com/bins/'+config.jsonDB, function (error, response, body) {
+      res = JSON.parse(body);
+      var exists = false;
+      if (error!=null) {
+        message.channel.sendMessage('\`ERROR: Could not access database\`');
+      }else {
+        for (var i = 0; i < res.user.length; i++) {
+          if (res.user[i].id===message.author.id) {
+            message.channel.sendMessage('\`ERROR: User already exists in database\`');
+            exists = true;
+          }
+        }
+        if (!exists) {
+          res.user.push({id: message.author.id, name:message.author.username, currency:5000});
+          request({ url: 'https://api.myjson.com/bins/'+config.jsonDB, method: 'PUT', json: res});
+          message.channel.sendMessage('Registered new wallet for @'+message.author.id);
+        }
+      }
+    });
+  },
+  wallet: function (args,message) {
+    request('https://api.myjson.com/bins/'+config.jsonDB, function (error, response, body) {
+      res = JSON.parse(body);
+      if (error!=null) {
+        message.channel.sendMessage('\`ERROR: Could not access database\`');
+      }else {
+        var exists = false;
+        if (args[0]) {
+          var id = args[0].replace(/\D/g, '');
+          for (var i = 0; i < res.user.length; i++) {
+            if (res.user[i].id===id) {
+              exists = true;
+              message.channel.sendMessage('Currency: $'+res.user[i].currency);
+            }
+          }
+          if (!exists) {
+            message.channel.sendMessage('\`ERROR: User not found in database | Use !register to get started!\`');
+          }
+        }else{
+          for (var i = 0; i < res.user.length; i++) {
+            if (res.user[i].id===message.author.id) {
+              exists = true;
+              message.channel.sendMessage('Currency: $'+res.user[i].currency);
+            }
+          }
+          if (!exists) {
+            message.channel.sendMessage('\`ERROR: User not found in database | Use !register to get started!\`');
+          }
+        }
+      }
+    });
+  },
+  walletadd: function (args,message) {
+    request('https://api.myjson.com/bins/'+config.jsonDB, function (error, response, body) {
+      var res = JSON.parse(body);
+      if (error!=null) {
+        message.channel.sendMessage('\`ERROR: Could not access database\`');
+      }else {
+        var exists = false;
+        if (args.length!=2) {
+          message.channel.sendMessage('\`ERROR: Incorrect Format => !walletadd <@user> <amount>\`');
+        }else {
+          var id = args[0].replace(/\D/g, '');
+          for (var i = 0; i < res.user.length; i++) {
+            if (res.user[i].id===id) {
+              exists = true;
+              res.user[i].currency += parseInt(args[1]);
+              message.channel.sendMessage('Added $'+parseInt(args[1])+' to '+message.mentions.users.get(id)+'\'s account | New Balance: $'+res.user[i].currency)
+              .then(request({ url: 'https://api.myjson.com/bins/'+config.jsonDB, method: 'PUT', json: res }));
+            }
+          }
+          if (!exists) {
+            message.channel.sendMessage('\`ERROR: User not found in database | Use !register to get started!\`');
+          }
+        }
+      }
+    });
+  }
+};
