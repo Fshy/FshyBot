61a45254f0165248f724f64454def07edef86ffe
Fshy <akdesignz@outlook.com>
2017-04-01 18:54:30 -0400

    Utilize Switch Case for Commands

--- app.js
+++ app.js
@@ -27,81 +27,74 @@ client.on('ready', () => {
 client.on('message', (message)=>{
   if(message.author.bot && !message.content.startsWith(config.prefix+'play')) return;
   if(!message.content.startsWith(config.prefix)) return;
-
   console.log("-- "+message.author.username+": "+message.content);
 
-  let command = message.content.split(/\s+/g)[0];
-  command = command.slice(config.prefix.length);
-  let args = message.content.split(/\s+/g).slice(1);
+  let command = message.content.split(/\s+/g)[0].slice(config.prefix.length);
+  let args    = message.content.split(/\s+/g).slice(1);
 
-  // !help = Displays all available commands
-  if (command === 'help') {
-    commands.help(message);
-  }else
-
-  // !ping = Displays latency between the bot and the server
-  if (command === 'ping') {
-    message.channel.sendEmbed({description: 'Response time to discord server: '+(Date.now()-message.createdTimestamp)+'ms',color: 15514833});
-  }else
-
-  // !uptime = Displays time since launch
-  if (command === 'uptime') {
-    commands.uptime(launchTime,message);
-  }else
-
-  // !btc - Displays current Bitcoin spot price
-  if (command === 'btc') {
-    commands.btc(message);
-  }else
-
-  // !eth - Displays current Ethereum spot price
-  if (command === 'eth') {
-    commands.eth(message);
-  }else
-
-  // !playlist <playlistId> - Queues all videos from a youtube playlist
-  if (command === 'playlist') {
-    commands.playlist(args,message);
-  }else
-
-  // !roll <# of sides> <# of dice>
-  if (command === 'roll') {
-    commands.roll(args,message);
-  }else
-
-  // !r - Uploads a random image from the frontpage of a given subreddit
-  if (command === 'r') {
-    if (args[0]) {
-      commands.getSubredditImages(reddit,message,args[0]);
-    }else {
-      message.channel.sendEmbed({description: 'ERROR: No subreddit specified | Use !r [subreddit]',color: 15514833});
-    }
-  }else
-
-  // !2B - Uploads a random safe 2B image from danbooru
-  if (command === '2B' || command === '2b') {
-    if(args[0]==='nsfw')
-      commands.danbooru('yorha_no._2_type_b','e',100,message);
-    else
-      commands.danbooru('yorha_no._2_type_b','s',100,message);
-  }else
-
-  // !lewd <search term> - Searches danbooru for a specific tag, and uploads a random nsfw image
-  if (command === 'lewd') {
-    var tag = args.join('_');
-    commands.danbooru(tag,'e',100,message);
-  }else
-
-  // !sfw <search term> - Searches danbooru for a specific tag, and uploads a random safe image
-  if (command === 'sfw') {
-    var tag = args.join('_');
-    commands.danbooru(tag,'s',100,message);
-  }else
-
-  // !tags <search term> - Searches danbooru for possible related search tags
-  if (command === 'tags') {
-    var tag = args.join('_');
-    commands.danbooruTags(tag,message);
+  switch (command) {
+    // !help = Displays all available commands
+    case 'help':
+      commands.help(message);
+      break;
+    // !ping = Displays latency between the bot and the server
+    case 'ping':
+      message.channel.sendEmbed({description: 'Response time to discord server: '+(Date.now()-message.createdTimestamp)+'ms',color: 15514833});
+      break;
+    // !uptime = Displays time since launch
+    case 'uptime':
+      commands.uptime(launchTime,message);
+      break;
+    // !btc - Displays current Bitcoin spot price
+    case 'btc':
+      commands.btc(message);
+      break;
+    // !eth - Displays current Ethereum spot price
+    case 'eth':
+      commands.eth(message);
+      break;
+    // !playlist <playlistId> - Queues all videos from a youtube playlist
+    case 'playlist':
+      commands.playlist(args,message);
+      break;
+    // !roll <# of sides> <# of dice>
+    case 'roll':
+      commands.roll(args,message);
+      break;
+    // !r - Uploads a random image from the frontpage of a given subreddit
+    case 'r':
+      if (args[0]) {
+        commands.getSubredditImages(reddit,message,args[0]);
+      }else {
+        message.channel.sendEmbed({description: 'ERROR: No subreddit specified | Use !r [subreddit]',color: 15514833});
+      }
+      break;
+      case 'r':
+    // !2B - Uploads a random safe 2B image from danbooru
+    case '2B':
+    case '2b':
+      if(args[0]==='nsfw')
+        commands.danbooru('yorha_no._2_type_b','e',100,message);
+      else
+        commands.danbooru('yorha_no._2_type_b','s',100,message);
+      break;
+    // !lewd <search term> - Searches danbooru for a specific tag, and uploads a random nsfw image
+    case 'lewd':
+      var tag = args.join('_');
+      commands.danbooru(tag,'e',100,message);
+      break;
+    // !sfw <search term> - Searches danbooru for a specific tag, and uploads a random safe image
+    case 'sfw':
+      var tag = args.join('_');
+      commands.danbooru(tag,'s',100,message);
+      break;
+    // !tags <search term> - Searches danbooru for possible related search tags
+    case 'tags':
+      var tag = args.join('_');
+      commands.danbooruTags(tag,message);
+      break;
+    default:
+      message.channel.sendEmbed({description: 'A-Are you talking to me? Because that\'s not a command I understand..\nReference !help to see what I can do, or adjust the prefix I listen for.',color: 15514833});
   }
 
 });
